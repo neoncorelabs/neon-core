d2e67f884e6f083316d74b15831081f2a2a49e68
monolithhq <studio.monolith@proton.me>
2026-07-27 09:32:27 +0200

    Fix missing getValue import for State delegate in StatusHeader

--- src/main/kotlin/neoncore/components/StatusHeader.kt
+++ src/main/kotlin/neoncore/components/StatusHeader.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
